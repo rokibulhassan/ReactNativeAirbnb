e03ac36173d63165a936ea9f9b913c9e59dc9a97
Rokibul Hasan <rakib063049@gmail.com>
2017-11-27 19:37:36 +0600

    Say hello world from miami app

---
+++ src/ExploreMiami/index.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+import './style.css'
+
+
+export default class ExploreMiami extends Component {
+    render() {
+        return (
+            <div className="main ui text container">
+                <h1 className="ui dividing centered header">Explore Miami</h1>
+                <div id="content">
+                    <h1> Hello world!</h1>
+                </div>
+            </div>
+        )
+    }
+}
