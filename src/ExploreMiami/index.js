62220c93c4f017386a035885087c87cebfb5266f
Rokibul Hasan <rakib063049@gmail.com>
2017-11-28 11:31:38 +0600

    Build html dom elements

--- src/ExploreMiami/index.js
+++ src/ExploreMiami/index.js
@@ -5,10 +5,45 @@ import './style.css'
 export default class ExploreMiami extends Component {
     render() {
         return (
-            <div className="main ui text container">
-                <h1 className="ui dividing centered header">Explore Miami</h1>
-                <div id="content">
-                    <h1> Hello world!</h1>
+            <div className="main ui container">
+                <h1 className="ui dividing header">Explore Miami</h1>
+                <div className="ui four column grid">
+                    <div className="column">
+                        <img className="ui wireframe image" src="https://a0.muscache.com/im/pictures/7bd0b8ea-34c8-4633-a7d4-9252107db617.jpg?aki_policy=large" alt="Item"/>
+                        <div className="category">Food Tasting</div>
+                        <div className="description">Miami Beach: Taste Your Way Through The Art Deco District</div>
+                        <div className="price">$59 per person</div>
+                    </div>
+                    <div className="column">
+                        <img className="ui wireframe image" src="https://a0.muscache.com/im/pictures/7bd0b8ea-34c8-4633-a7d4-9252107db617.jpg?aki_policy=large" alt="Item"/>
+                        <div className="category">Food Tasting</div>
+                        <div className="description">Miami Beach: Taste Your Way Through The Art Deco District</div>
+                        <div className="price">$59 per person</div>
+                    </div>
+                    <div className="column">
+                        <img className="ui wireframe image" src="https://a0.muscache.com/im/pictures/7bd0b8ea-34c8-4633-a7d4-9252107db617.jpg?aki_policy=large" alt="Item"/>
+                        <div className="category">Food Tasting</div>
+                        <div className="description">Miami Beach: Taste Your Way Through The Art Deco District</div>
+                        <div className="price">$59 per person</div>
+                    </div>
+                    <div className="column">
+                        <img className="ui wireframe image" src="https://a0.muscache.com/im/pictures/7bd0b8ea-34c8-4633-a7d4-9252107db617.jpg?aki_policy=large" alt="Item"/>
+                        <div className="category">Food Tasting</div>
+                        <div className="description">Miami Beach: Taste Your Way Through The Art Deco District</div>
+                        <div className="price">$59 per person</div>
+                    </div>
+                    <div className="column">
+                        <img className="ui wireframe image" src="https://a0.muscache.com/im/pictures/7bd0b8ea-34c8-4633-a7d4-9252107db617.jpg?aki_policy=large" alt="Item"/>
+                        <div className="category">Food Tasting</div>
+                        <div className="description">Miami Beach: Taste Your Way Through The Art Deco District</div>
+                        <div className="price">$59 per person</div>
+                    </div>
+                    <div className="column">
+                        <img className="ui wireframe image" src="https://a0.muscache.com/im/pictures/7bd0b8ea-34c8-4633-a7d4-9252107db617.jpg?aki_policy=large" alt="Item"/>
+                        <div className="category">Food Tasting</div>
+                        <div className="description">Miami Beach: Taste Your Way Through The Art Deco District</div>
+                        <div className="price">$59 per person</div>
+                    </div>
                 </div>
             </div>
         )
