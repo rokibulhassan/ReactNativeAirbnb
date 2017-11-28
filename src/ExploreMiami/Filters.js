6960a8813ac1477ccdcbb0e4f22c6d625b00c921
Rokibul Hasan <rakib063049@gmail.com>
2017-11-28 18:26:29 +0600

    Write category filter handler

--- src/ExploreMiami/Filters.js
+++ src/ExploreMiami/Filters.js
@@ -2,6 +2,11 @@ import React, {Component} from 'react'
 
 export default class Filters extends Component {
 
+    handleCategoryFilter = (category) => (
+        this.props.filterByCategory(category)
+    );
+
+
     render() {
         return (
             <div className="ui equal width grid filers">
@@ -11,21 +16,27 @@ export default class Filters extends Component {
                             <img className="ui wireframe image"
                                  src="https://a0.muscache.com/ac/pictures/8b7519ec-2c82-4c09-8233-fd4d2715bbf9.jpg?interpolation=lanczos-none&size=large&output-format=jpg&output-quality=70"/>
                         </div>
-                        <div className="filter-title">Homes</div>
+                        <div className="filter-title">
+                            <a onClick={this.handleCategoryFilter}>Homes</a>
+                        </div>
                     </div>
                     <div className="column">
                         <div className="filter-image">
                             <img className="ui wireframe image filter"
                                  src="https://a0.muscache.com/ac/pictures/d3811ff7-cc34-471b-8aee-b0d613db0052.jpg?interpolation=lanczos-none&size=large&output-format=jpg&output-quality=70"/>
                         </div>
-                        <div className="filter-title">Experiences</div>
+                        <div className="filter-title">
+                            <a onClick={this.handleCategoryFilter}>Experiences</a>
+                        </div>
                     </div>
                     <div className="column">
                         <div className="filter-image">
                             <img className="ui wireframe image filter"
                                  src="https://a0.muscache.com/ac/pictures/da2d8e97-90b7-409f-94ac-5ab0327c289b.jpg?interpolation=lanczos-none&size=large&output-format=jpg&output-quality=70"/>
                         </div>
-                        <div className="filter-title">Restaurants</div>
+                        <div className="filter-title">
+                            <a onClick={this.handleCategoryFilter}>Restaurants</a>
+                        </div>
                     </div>
                     <div className="column"></div>
                     <div className="column"></div>
